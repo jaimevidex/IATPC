a1abeef92bba3eaad1cb9239c2cbe0c1f1e8c42e
jaimevidex <145666480+jaimevidex@users.noreply.github.com>
2023-09-25 18:49:46 +0100

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,14 @@
+import searchalgorithm.*;
+import searchproblem.*;
+import undirectedgraph.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Graph graph = Romenia.defineGraph();
+        graph.showLinks();
+        graph.showSets();
+        Node n;
+        n = graph.searchSolution("Arad", "Bucharest", Algorithms.AStarSearch);
+        graph.showSolution(n);
+    }
+}
